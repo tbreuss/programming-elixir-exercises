a88cc50279de64b9ddaebd265743afb3838107da
tbreuss <thomasbreuss@gmx.ch>
2019-04-11 21:36:21 +0200

    Added version using enum methods

--- chapter-11/strings-and-binaries-2.exs
+++ chapter-11/strings-and-binaries-2.exs
@@ -1,4 +1,6 @@
 defmodule MyString do
+
+  # version using recursion
   def anagram?(str1, str2) do
     _anagram?(
       str1
@@ -17,7 +19,28 @@ defmodule MyString do
       false
     end
   end
+
+  # version using Enum methods
+  def anagram2?(str1, str2) do
+    transform(str1) === transform(str2)
+  end
+  defp transform(string) when is_list(string) do
+    string
+      |> List.to_string
+      |> transform
+  end
+  defp transform(string) do
+    string
+      |> String.downcase
+      |> String.to_charlist
+      |> Enum.sort
+  end
 end
 
 IO.inspect MyString.anagram?("Elvis","Lives")  # true
 IO.inspect MyString.anagram?("Apple","Banana") # false
+
+IO.inspect MyString.anagram2?('elvis',"lives")  # true
+IO.inspect MyString.anagram2?('apple','banana') # false
+IO.inspect MyString.anagram2?("Elvis",'LIVES')  # true
+IO.inspect MyString.anagram2?('Apple',"Banana") # false
