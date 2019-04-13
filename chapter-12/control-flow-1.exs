e7a2658dfadcb0278fefecfbbfb9229e9ed6f3ec
tbreuss <thomasbreuss@gmx.ch>
2019-04-13 10:45:08 +0200

    Code formatting

--- chapter-12/control-flow-1.exs
+++ chapter-12/control-flow-1.exs
@@ -5,7 +5,7 @@ defmodule FizzBuzzWithCase do
   defp fizzbuzz(n) do
     case n do
       n when rem(n, 3) == 0 and rem(n, 5) == 0 -> "FizzBuzz"
-      n when rem(n,3) == 0 -> "Fizz"
+      n when rem(n, 3) == 0 -> "Fizz"
       n when rem(n, 5) == 0 -> "Buzz"
       _ -> n
     end
