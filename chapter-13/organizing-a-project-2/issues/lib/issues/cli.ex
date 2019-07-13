692d51539263a6c1c1f34af14c67281a83476e3c
tbreuss <thomasbreuss@gmx.ch>
2019-07-13 10:53:01 +0200

    Added chapter 13 / organizing a project 2

---
+++ chapter-13/organizing-a-project-2/issues/lib/issues/cli.ex
@@ -0,0 +1,50 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to​
+  the various functions that end up generating a​
+  table of the last _n_ issues in a github project​
+  """
+
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.​
+
+  Otherwise it is a github user name, project name, and (optionally)​
+  the number of entries to format.​​ 	
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.​
+  """
+  def parse_args(argv) do
+    parse =
+      OptionParser.parse(
+        argv,
+        switches: [help: :boolean],
+        aliases: [h: :help]
+      )
+
+    case parse do
+      {[help: true], _, _} -> :help
+      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
+      {_, [user, project], _} -> {user, project, @default_count}
+      _ -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+end
