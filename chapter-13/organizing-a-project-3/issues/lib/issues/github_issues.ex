6bdf78b6434e13b051467c3c5e9506da1e832f53
tbreuss <thomasbreuss@gmx.ch>
2019-07-13 11:34:29 +0200

    Added chapter 13 / organizing a project 3

---
+++ chapter-13/organizing-a-project-3/issues/lib/issues/github_issues.ex
@@ -0,0 +1,22 @@
+defmodule Issues.GithubIssues do
+  @github_url Application.get_env(:issues, :github_url)
+  @user_agent [{"User-agent", "Elixir dave@progprog.com"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body, %{})}
+  end
+
+  def heandle_response({_, %{status_code: _, body: body}}) do
+    {:error, Poison.Parser.parse!(body, %{})}
+  end
+end
