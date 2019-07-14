a18b91bcc94871d2388f93eb79d2943b29665f55
tbreuss <thomasbreuss@gmx.ch>
2019-07-14 18:16:06 +0200

    Added chapter 13 / organizing a project 4

---
+++ chapter-13/organizing-a-project-4/issues/test/cli_test.exs
@@ -0,0 +1,30 @@
+defmodule CLITest do
+  use ExUnit.Case
+  doctest Issues.CLI
+
+  import Issues.CLI, only: [parse_args: 1, sort_into_ascending_order: 1]
+
+  test "help returned by option parsing with -h an --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
+  end
+
+  test "count is defaulted if two values given" do
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "sort ascending orders the correct way" do
+    result = sort_into_ascending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{a b c}
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+        do: %{"created_at" => value, "other_data" => "xxx"}
+  end
+end
