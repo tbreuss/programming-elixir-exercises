638f7f5eece570662c94fc07959ecd7413b18db7
tbreuss <thomasbreuss@gmx.ch>
2019-07-15 21:18:37 +0200

    Using logger with different log levels

--- chapter-13/organizing-a-project-4/issues/lib/issues/github_issues.ex
+++ chapter-13/organizing-a-project-4/issues/lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @github_url Application.get_env(:issues, :github_url)
   @user_agent [{"User-agent", "Elixir dave@progprog.com"}]
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -12,7 +16,9 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
+  def handle_response({:ok, %{status_code: status_code, body: body}}) do    
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
     {:ok, Poison.Parser.parse!(body, %{})}
   end
 
